508e0d1596bd0ec4f7c8b3e3450cb5bfeb9c7929
ng-family <pcng@outlook.com>
2020-08-24 06:43:13 -0600

    Corrected some lines where bash was splitting strings if there was a space

--- Convert1080p.sh
+++ Convert1080p.sh
@@ -4,17 +4,14 @@
 #
 # This script takes a mkv container file and output a mkv compressed to a specific quality.
 # 
-# TO DO:
-# Allow arguments to pass audio and subtitle selections to use.  Main English audio, subtitles, and forced subtitles.
 
-# handbrake_options="--markers --large-file --encoder x264 --encopts vbv-maxrate=25000:vbv-bufsize=31250:ratetol=inf --crop 0:0:0:0 --strict-anamorphic"
-
+## Error handler
 except() {
 	echo "Error: $1" >&2
 	exit ${2:-1}
 }
 
-# Parse arguments
+## Parse arguments
 case $1 in
 	-h|--help)
 	echo "Usage: convert1080p.sh -i {input.mkv} -o {output.mkv} -s {main subtitle track number}"
@@ -59,7 +56,7 @@ case $argv in
 esac
 done
 
-#Required arguments
+## Required arguments
 if [ ! "$inputfile" ]; then
 	except "Missing source file"
 fi
@@ -70,24 +67,21 @@ if [ ! "$audiotrack" ]; then
 	except "Missing audio track"
 fi
 
+## Handbrake Options for 1080
 video_options="--encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 4.0 -q 20 -2 --pfr"
 encoder_options="ref=5:bframes=5" # Taken from superHQ profile
 # encoder_options="vbv-maxrate=25000:vbv-bufsize=31250:ratetol=inf" #dev
-
-audio_options="-a $audiotrack -E av_aac,ac3,copy:dtshd -6 stereo,5point1 -A \"Stereo,AC3\ Surround\ 5.1,Surround\ 5.1\""
+audio_options="-a "$audiotrack" -E av_aac,ac3,copy:dtshd -6 stereo,5point1 -A \"Stereo,AC3\ Surround\ 5.1,Surround\ 5.1\""
 picture_options="--auto-anamorphic" #--crop auto is default --modulus 2 is default
 filters_options="" #profile is all default
 subtitles_options=""
 if [ "$subtitletrack" ]; then
-	subtitles_options="-s $subtitletrack" #-s 1,2,3 --subtitle_burned 2
+	subtitles_options="-s "$subtitletrack"" #expecting a string like "1,1,2"
 fi
 if [ "$forcedsubtitletrack" ]; then
-	subtitles_options="$subtitles_options --subtitle_burned $forcedsubtitletrack"
+	subtitles_options=""$subtitles_options" --subtitle_burned "$forcedsubtitletrack"" #expecting a string like "2"
 fi
 
-### Encode Video
+## Encode Video
+time HandBrakeCLI "$video_options" --encopts "$encoder_options" "$audio_options" "$subtitles_options" --input "$inputfile" --output "$outputfile" 2>&1
 
-#echo "HandBrakeCLI $video_options --encopts $encoder_options $audio_options $subtitles_options --input $inputfile --output $outputfile 2>&1"
-time HandBrakeCLI $video_options --encopts $encoder_options $audio_options $subtitles_options --input "$inputfile" --output "$outputfile" 2>&1
-
-#HandBrakeCLI -encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 4.0 -x ref=5:bframes=5 -q 20 -2 --pfr -E av_aac,ac3,copy:dtshd -6 "stereo,5point1" -A "Stereo,AC3 Surround 5.1,Surround 5.1" -s 1 -i THE\ SPY\ WHO\ LOVED\ ME_t00.mkv -o The.mkv
