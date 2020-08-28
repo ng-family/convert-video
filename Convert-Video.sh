64d3a1e70d06f6fc3a4ffc09296a4f0215b63913
ng-family <pcng@outlook.com>
2020-08-28 06:48:11 -0600

    Urgh... it's running again.

--- Convert-Video.sh
+++ Convert-Video.sh
@@ -70,37 +70,44 @@ fi
 ## Determine Video Size and apply appropriate video option
 scanresults=$(HandBrakeCLI --input "$inputfile" --scan 2>&1)
 videostream=$(grep -P '(?<=Stream).*[0-9]+x[0-9]+' <<< "$scanresults")
-videoheight=$(grep -oP '(?<=[0-9]{3}x)[0-9]+' <<< "$videostream")
+videoheight=0
+while IFS= read -r line
+do
+	height=$(grep -oP -m 1 '(?<=[0-9]{3}x)[0-9]+' <<< "$line")
+	if [ -z "$height" ]; then
+		echo "$height is empty"
+	elif (($height > $videoheight)); then
+		videoheight=$height
+	fi
+done <<< "$videostream"
 if (($videoheight < 484)); then
-	video_options="--encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 4.0 -q 20 -2 --pfr"
-	encoder_options="ref=5:bframes=5" # Taken from superHQ profile
-	audio_options="-a "$audiotrack" -E av_aac,ac3,copy -6 stereo,5point1 -A \"Stereo,AC3\ Surround\ 5.1,Surround\ 5.1\""
+	video_options="--encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 3.1 -q 16 -2 --pfr"
+	audio_options="-a $audiotrack -aencode av_aac,copy -6 stereo -A Stereo,\"Surround\ 5.1\""
 elif (($videoheight < 1090)); then
-	video_options="--encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 4.0 -q 20 -2 --pfr"
-	encoder_options="ref=5:bframes=5" # Taken from superHQ profile
-	audio_options="-a "$audiotrack" -E av_aac,ac3,copy -6 stereo,5point1 -A \"Stereo,AC3\ Surround\ 5.1,Surround\ 5.1\""
+	video_options='--encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 4.0 -q 20 -2 --pfr'
+	audio_options="-a $audiotrack -aencode av_aac,ac3,copy -6 stereo,5point1 -A Stereo,\"AC3\ Surround\ 5.1,Surround\ 5.1\""
 else
+	#I'm debating of implementing AV1 here since 4k movies are soooo large
 	video_options="--encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 4.0 -q 20 -2 --pfr"
-	encoder_options="ref=5:bframes=5" # Taken from superHQ profile
-	audio_options="-a "$audiotrack" -E av_aac,ac3,copy -6 stereo,5point1 -A \"Stereo,AC3\ Surround\ 5.1,Surround\ 5.1\""
+	audio_options="-a "$audiotrack" -E av_aac,ac3,copy -6 stereo,5point1 -A \"Stereo,AC3 Surround 5.1,Surround 5.1\""
 fi
-exit
 
-## Handbrake Options for 1080
-#video_options="--encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 4.0 -q 20 -2 --pfr"
-#encoder_options="ref=5:bframes=5" # Taken from superHQ profile
+## Manual Handbrake Options Override
+# video_options="--encoder x264 --encoder-preset VerySlow --encoder-profile High --encoder-level 4.0 -q 20 -2 --pfr"
+encoder_options="ref=5:bframes=5" # Taken from superHQ profile
 # encoder_options="vbv-maxrate=25000:vbv-bufsize=31250:ratetol=inf" #dev
-#audio_options="-a "$audiotrack" -E av_aac,ac3,copy -6 stereo,5point1 -A \"Stereo,AC3\ Surround\ 5.1,Surround\ 5.1\""
+# audio_options="-a $audiotrack --aencode ca_aac,copy -6 stereo -A Stereo,\"Surround 5.1\""
 picture_options="--auto-anamorphic" #--crop auto is default --modulus 2 is default
 filters_options="" #profile is all default
 subtitles_options=""
 if [ "$subtitletrack" ]; then
-	subtitles_options="-s "$subtitletrack"" #expecting a string like "1,1,2"
+	subtitles_options="-s $subtitletrack" #expecting a string like "1,1,2"
 fi
 if [ "$forcedsubtitletrack" ]; then
-	subtitles_options=""$subtitles_options" --subtitle_burned "$forcedsubtitletrack"" #expecting a string like "2"
+	subtitles_options="$subtitles_options --subtitle_burned $forcedsubtitletrack" #expecting a string like "2"
 fi
 
 ## Encode Video
-time HandBrakeCLI "$video_options" --encopts "$encoder_options" "$audio_options" "$subtitles_options" --input "$inputfile" --output "$outputfile" 2>&1
+echo "HandBrakeCLI "$video_options" --encopts "$encoder_options" "$audio_options" "$subtitles_options" --input "$inputfile" --output "$outputfile" 2>&1"
+time HandBrakeCLI $video_options --encopts $encoder_options $audio_options $subtitles_options --input "$inputfile" --output "$outputfile" 2>&1
 
